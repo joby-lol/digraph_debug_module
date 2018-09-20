72b4409d4a0e71fb176432af6fb2ad3fdf0fe2f7
Joby Elliott <joby@Jobys-iMac.local>
2018-09-20 11:26:27 -0600

    changing how user IDs work, renaming dump, adding dump to actions

--- routes/debug@all/root.php
+++ routes/debug@all/root.php
@@ -1,3 +1,3 @@
 <p>Visiting this page turns you into the user "root." You'll need to refresh to see it take effect.</p>
 <?php
-$package->cms()->helper('users')->id('null/root');
+$package->cms()->helper('users')->id('root@null');
