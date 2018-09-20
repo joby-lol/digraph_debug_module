e58eb328a0bab8d249a1d1fea8233a82244f24f0
Joby Elliott <joby@byjoby.com>
2018-09-20 14:00:38 -0600

    adjustments for new user system bits

--- routes/debug@all/user.php
+++ routes/debug@all/user.php
@@ -7,6 +7,7 @@ if (!($user = $this->helper('users')->user())) {
 <p>This page dumps info about the user object your current account gets built for it through the users helper.</p>
 <ul>
     <li>identifier(): <?php echo $user->identifier(); ?></li>
+    <li>id(): <?php echo $user->id(); ?></li>
     <li>name(): <?php echo $user->name(); ?></li>
     <li>email(): <?php echo $user->email(); ?></li>
 </ul>
