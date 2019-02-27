f60c7cb80d85e2c5da39261311e83608509accbd
Joby Elliott <elhober@unm.edu>
2019-02-27 08:36:09 -0700

    requireAuth() test page

--- routes/_debug@all/loginbounce.php
+++ routes/_debug@all/loginbounce.php
@@ -1,11 +1,8 @@
 <p>
-    This page calls the user helper's redirectToLogin() method. It should bounce
+    This page calls the user helper's requireAuth() method. It should bounce
     you to the login page, and back here once you're signed in.
 </p>
 <?php
-if ($this->helper('users')->isAuthenticated()) {
-    return;
-}
-$this->package()->response()->set('cacheable', false);
-$this->helper('users')->redirectToLogin();
+$package->noCache();
+$this->helper('users')->requireAuth($package);
  ?>
